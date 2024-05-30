301fc4a08727c7e49d8544f8b940d2f5421de9a5
guilhermeSM616 <72474720+guilhermeSM616@users.noreply.github.com>
2024-05-30 12:58:10 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bem-vindo ao meu App!</h1>
+      <Button variant="contained" color="primary">
+        Clique Aqui
+      </Button>
+    </div>
+  );
+}
+
+export default App;
